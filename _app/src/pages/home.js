25fb9cda00aaacebaf2bdc95312ae0bd7e822884
Haroen Viaene <hello@haroen.me>
2017-01-14 17:49:47 +0100

    untest

--- _app/src/pages/home.js
+++ _app/src/pages/home.js
@@ -3,7 +3,6 @@ import html from 'choo/html'
 export default (state, prev, send) => html`
   <main>
     <h1>Hello, World!</h1>
-    <a href="test">test</a>
     <p>If you are seeing this, then the generator works!</p>
     <h2>Demo</h2>
     <h3>${state.title}</h3>
